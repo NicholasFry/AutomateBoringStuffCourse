8db166a516f045fd2e9e7021f2438ed90f072cfa
Nick Fry <nicholas18@ru.is>
2021-07-02 21:49:25 +0000

    lesson1+

---
+++ lesson1.py
@@ -0,0 +1,19 @@
+# spam = 42
+# spam = "Hello"
+# print(spam + " World")
+# spam = 2+2
+# spam = 10
+# spam = spam + 1
+# print(spam)
+
+#This program says hello and asks for their name
+print("hello world")
+#ask for name
+print("What is your name?")
+myName = input()
+print("It is good to meet you, " + myName)
+print("The length of your name is " + str(len(myName)))
+#ask for age
+print("What is your age?")
+myAge = input()
+print("You will be "+ str(int(myAge) + 1) + " in a year.")
